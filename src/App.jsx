4e0e68a459cf11d54681506accd2c9ef321541e1
Mana1010 <tristanvicclarito2003@gmail.com>
2023-07-05 12:26:18 +0800

    Beshy App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import GroceryBuggy from './GroceryBuggy'
+import Beshy from './Beshy'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount ] = useState(0);
   return (
     <>
-    <GroceryBuggy/>
+    <Beshy/>
     </>
   )
+
 }
 
 export default App
+
+
